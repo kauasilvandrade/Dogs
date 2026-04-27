7a8bca818eff4972bb429cabc69e36ea6e714755
Kauã Andrade <kauasilvaz@hotmail.com>
2026-04-27 11:42:34 -0300

    cria estrurua inicial do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router";
+import Dogs from "../../assets/dogs.svg";
+import User from "../../assets/usuario.svg";
 
 export function Header() {
   return (
-    <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/login"}>Login / Criar</Link>
-    </nav>
+    <header className="shadow fixed w-full z-10 bg-white top-0">
+      <nav className="container h-16 flex justify-between items-center">
+        <Link to={"/"} className="px-0 py-2">
+          <img src={Dogs} aria-label="Dogs - Home" alt="Ícone Dogs" />
+        </Link>
+        <Link to={"/login"} className="text-[#333] flex items-center gap-2">
+          Login / Criar
+          <img src={User} className="w-[14px] h-[17px]" />
+        </Link>
+      </nav>
+    </header>
   );
 }
