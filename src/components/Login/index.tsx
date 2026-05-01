e0c02776fc76a1856409277dc631c76cae5b8339
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2026-05-01 12:15:45 -0300

    style: estilos do formulário de login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -6,18 +6,25 @@ import { LoginPasswordReset } from "./LoginPasswordReset";
 import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 
+import img from "./../../assets/login.jpg";
+
 export function Login() {
   const { login } = useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/criar" element={<LoginCreate />} />
-        <Route path="/perdeu" element={<LoginPasswordLost />} />
-        <Route path="/resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section
+      style={{ "--bg-img": `url(${img})` } as React.CSSProperties}
+      className={`grid grid-cols-2 min-h-vh gap-8 before:content-[''] before:block before:bg-[image:var(--bg-img)] before:bg-no-repeat before:bg-center before:bg-cover max-sm:grid-cols-1 max-sm:before:none`}
+    >
+      <div className="max-w-[30rem] p-4 max-sm:max-w-full">
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/criar" element={<LoginCreate />} />
+          <Route path="/perdeu" element={<LoginPasswordLost />} />
+          <Route path="/resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 }
